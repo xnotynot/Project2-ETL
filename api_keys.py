b61991d5fbb64f9376ee2e7b9bd25a6c1beb559d
Anand Shankar <anand.shankar.r@outlook.com>
2022-12-17 23:52:05 -0500

    20221217

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,6 @@
 # OpenWeatherMap API Key
 weather_api_key = "5beb16aa8a0be4c685a25ae9ffdf8b71"
+postgres_user = 'postgres'
+postgres_pwd = 'admin'
+postgres_host = 'localhost'
+postgres_port = '5432'
