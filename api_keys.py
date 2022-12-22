14a9e4ede42d927287dfeafb40625c8083951537
anands <94428256+xnotynot@users.noreply.github.com>
2022-12-21 19:46:23 -0500

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key and postgre sql connection params
-weather_api_key = "5beb16aa8a0be4c685a25ae9ffdf8b71"
-postgres_user = 'postgres'
-postgres_pwd = 'admin'
-postgres_host = 'localhost'
-postgres_port = '5432'
